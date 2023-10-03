7f28d014d2ca2c9c57cf528f37005370a397cb1d
Rabik Thapa <rabikthapa00@gmail.com>
2023-10-03 16:56:46 +0545

    added new testing

--- Automation/cypress.config.js
+++ Automation/cypress.config.js
@@ -1,7 +1,11 @@
 module.exports = {
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+      require('cypress-mochawesome-reporter/plugin')(on);
+    }, 
+    
+
   },
 };
---
+++ Automation/cypress/e2e/2-advanced-examples/appa.cy.js
@@ -0,0 +1,9 @@
+describe('Google', () => {
+    it('should visit Google', () => {
+      // Visit the Google website
+      cy.visit('https://ecommerce.tech-glazers.com/');
+  
+     
+    });
+  });
+  
